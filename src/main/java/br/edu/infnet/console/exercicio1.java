1c11c17ba6c629fd014698fe47940bb4f9d53089
filipe.ferreira <filipe.ferreira@sistemaprismatec.com.br>
2025-06-01 20:00:14 -0300

    TP3

---
+++ src/main/java/br/edu/infnet/console/exercicio1.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.console;
+
+import br.edu.infnet.integration.apichallenges.ApiEntitiesClient;
+
+public class exercicio1 {
+    public static void main(String[] args) throws Exception {
+        ApiEntitiesClient.exercicio1ObterTodasAsEntidades();
+    }
+}
