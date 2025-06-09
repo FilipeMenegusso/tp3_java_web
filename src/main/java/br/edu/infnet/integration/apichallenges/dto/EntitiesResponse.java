4ccb9cb3a3ecd84d5e7e06ea429258b1b8c0f59b
filipe.ferreira <filipe.ferreira@sistemaprismatec.com.br>
2025-06-09 14:11:11 -0300

    TP3

--- src/main/java/br/edu/infnet/integration/apichallenges/dto/EntitiesResponse.java
+++ src/main/java/br/edu/infnet/integration/apichallenges/dto/EntitiesResponse.java
@@ -4,17 +4,13 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class EntityResponse {
+public class EntitiesResponse {
     public List<Entity> entities;
 
     public List<Entity> getEntities() {
         return entities.stream()
                 .sorted(Comparator.comparing(Entity::getId))
                 .collect(Collectors.toList());
-    }
-
-    public void setEntities(List<Entity> entities) {
-        this.entities = entities;
     }
 }
 
